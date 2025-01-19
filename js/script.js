c131119d4c0dc91e9b2ed6912014983bb58f5482
Marina <marinamrtnzal@gmail.com>
2025-01-19 23:45:15 +0100

    el final mas lamentablexdd

--- js/script.js
+++ js/script.js
@@ -4,4 +4,31 @@
 
  menuButton.addEventListener("click", () => {
      nav.classList.toggle("movil");
+     console.log("boton")
  });
+
+
+ // SCRIPT AMPLIACION IMAGENES GALERIA
+const galleryImages = document.querySelectorAll('div.gallery img');
+
+const overlay = document.getElementById('overlay');
+const largeImage = document.getElementById('large-image');
+const closeOverlay = document.getElementById('close-overlay');
+
+
+galleryImages.forEach(image => {
+    image.addEventListener('click', (e) => {
+      
+        largeImage.src = e.target.src;
+    
+        overlay.style.display = 'flex';
+    });
+});
+
+
+// Cerrar el overlay si se hace clic fuera de la imagen
+overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+        overlay.style.display = 'none';
+    }
+});
